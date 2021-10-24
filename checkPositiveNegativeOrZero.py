047631ca7c2174e149ff2990353a8dd1d49cd1ce
Sahil Gupta <sahil.gupta@geeksforgeeks.org>
2021-10-24 16:42:21 +0530

    positive negative or zero

---
+++ checkPositiveNegativeOrZero.py
@@ -0,0 +1,9 @@
+# Check if else structure properly
+num = float(input("Enter the number: "))
+
+if num > 0:
+   print("Positive number")
+elif num == 0:
+   print("Zero")
+else:
+   print("Negative number")
