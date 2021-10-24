f4eaf2718c7a217fe629860829dfa04e92fb1961
Sahil Gupta <sahil.gupta@geeksforgeeks.org>
2021-10-24 16:25:59 +0530

    area of triangle

---
+++ areaOfRectangle.py
@@ -0,0 +1,16 @@
+# Are of a right angled triangle is 1/2 * Base * Height
+
+base = input("Enter the length of the base of the triangle ")
+height = input("Enter the height of the triangle ")
+
+# Just type converting the base and the heights to float type
+base = float(base)
+height = float(height)
+
+area = 1 / 2 * base * height
+
+print("Area of the triangle is ", area)
+
+
+# Now this program can also be written for other types of triangles 
+# but only formula change will fix the above program for a general triangle
