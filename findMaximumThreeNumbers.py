88f880725964b20e083cdf93fd24b87b5bc4bc3e
Sahil Gupta <sahil.gupta@geeksforgeeks.org>
2021-11-18 15:54:25 +0530

    day 5

---
+++ findMaximumThreeNumbers.py
@@ -0,0 +1,13 @@
+def findGreater(num1, num2, num3):
+    if num1 > num2 and num1 > num3:
+        print(num1, " is the greatest")
+    elif num2 > num1 and num2 > num3:
+        print(num2, " is the greatest")
+    else:
+        print(num3, " is the greatest")
+
+
+num1 = int(input("Enter the first number "))
+num2 = int(input("Enter the second number "))
+num3 = int(input("Enter the third number "))
+findGreater(num1, num2, num3)
