020054f72817e2adf10ddd00464c0efc25aeedff
Sahil Gupta <sahil.gupta@geeksforgeeks.org>
2021-11-27 16:47:33 +0530

    day-9

---
+++ reverseCounting.py
@@ -0,0 +1,6 @@
+def printReverse():
+    # a hme sif 1 tak jaana hai islie range function me second item 0 rakha hai
+    for i in range(100, 0, -1):
+        print(i)
+
+printReverse()
---
+++ tableOf5Func.py
@@ -0,0 +1,5 @@
+def printTable():
+    for i in range(1, 11):
+        print("5 X ", i, " = ", 5 * i)
+
+printTable()
